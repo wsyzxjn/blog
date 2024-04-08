6396319237140071a8be5e28251bceb5b60b6377
Amatsuka <45129539+wsyzxjn@users.noreply.github.com>
2024-04-08 15:36:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://wsyzxjn.github.io",
-  base: "/",
+  base: "/blog",
   integrations: [
     tailwind(),
     swup({
